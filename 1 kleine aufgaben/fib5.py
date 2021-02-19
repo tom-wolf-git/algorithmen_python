333ccee9f5f69bdfdd5d9a64946a92168d8b57de
tom-wolf-git <tom_wolf@me.com>
2021-02-19 22:25:06 +0100

    20210219

--- 1 kleine aufgaben/fib5.py
+++ 1 kleine aufgaben/fib5.py
@@ -1,18 +1,5 @@
 # fib5.py
-# Aus "Algorithmen in Python", Kapitel 1
-# Copyright 2018 David Kopec
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 def fib5(n: int) -> int:
     if n == 0: return n  # Spezialfall
     last: int = 0  # Am Anfang auf fib(0) setzen
@@ -25,4 +12,3 @@ def fib5(n: int) -> int:
 if __name__ == "__main__":
     print(fib5(2))
     print(fib5(50))
-
